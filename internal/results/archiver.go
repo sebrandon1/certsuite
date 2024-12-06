ead28a8cb6a8aeca76782482b86474146c8951b4
Brandon Palm <bpalm@redhat.com>
2024-12-06 16:28:03 -0600

    Add upload of tar.gz to Red Hat Connect endpoint

--- internal/results/archiver.go
+++ internal/results/archiver.go
@@ -2,9 +2,12 @@ package results
 
 import (
 	"archive/tar"
+	"bytes"
 	"compress/gzip"
 	"fmt"
 	"io"
+	"mime/multipart"
+	"net/http"
 	"os"
 	"path/filepath"
 	"time"
@@ -16,6 +19,9 @@ const (
 	// tarGz file prefix layout format: YearMonthDay-HourMinSec
 	tarGzFileNamePrefixLayout = "20060102-150405"
 	tarGzFileNameSuffix       = "cnf-test-results.tar.gz"
+
+	// Connect API Information
+	connectAPIURL = "https://access.qa.redhat.com/hydra/cwe/rest/v1.0/attachments/upload"
 )
 
 func generateZipFileName() string {
@@ -38,14 +44,14 @@ func getFileTarHeader(file string) (*tar.Header, error) {
 }
 
 // Creates a zip file in the outputDir containing each file in the filePaths slice.
-func CompressResultsArtifacts(outputDir string, filePaths []string) error {
+func CompressResultsArtifacts(outputDir string, filePaths []string) (string, error) {
 	zipFileName := generateZipFileName()
 	zipFilePath := filepath.Join(outputDir, zipFileName)
 
 	log.Info("Compressing results artifacts into %s", zipFilePath)
 	zipFile, err := os.Create(zipFilePath)
 	if err != nil {
-		return fmt.Errorf("failed creating tar.gz file %s in dir %s (filepath=%s): %v",
+		return "", fmt.Errorf("failed creating tar.gz file %s in dir %s (filepath=%s): %v",
 			zipFileName, outputDir, zipFilePath, err)
 	}
 
@@ -60,24 +66,106 @@ func CompressResultsArtifacts(outputDir string, filePaths []string) error {
 
 		tarHeader, err := getFileTarHeader(file)
 		if err != nil {
-			return err
+			return "", err
 		}
 
 		err = tarWriter.WriteHeader(tarHeader)
 		if err != nil {
-			return fmt.Errorf("failed to write tar header for %s: %v", file, err)
+			return "", fmt.Errorf("failed to write tar header for %s: %v", file, err)
 		}
 
 		f, err := os.Open(file)
 		if err != nil {
-			return fmt.Errorf("failed to open file %s: %v", file, err)
+			return "", fmt.Errorf("failed to open file %s: %v", file, err)
 		}
 
 		if _, err = io.Copy(tarWriter, f); err != nil {
-			return fmt.Errorf("failed to tar file %s: %v", file, err)
+			return "", fmt.Errorf("failed to tar file %s: %v", file, err)
 		}
 
 		f.Close()
+	}
+
+	// Return the entire path to the zip file
+	return zipFilePath, nil
+}
+
+func createFormField(w *multipart.Writer, field, value string) error {
+	_, err := w.CreateFormField(field)
+	if err != nil {
+		return fmt.Errorf("failed to create form field: %v", err)
+	}
+
+	err = w.WriteField(field, value)
+	if err != nil {
+		return fmt.Errorf("failed to write field %s: %v", field, err)
+	}
+
+	return nil
+}
+
+// curl example:
+// curl --location 'https://access.qa.redhat.com/hydra/cwe/rest/v1.0/attachments/upload' \
+// --header 'x-api-key: API_KEY' \
+// --form 'type="RhocpBestPracticeTestResult"' \
+// --form 'attachment=@"/Users/yangli/Downloads/rhocp-best-practice-test-results/20240925-143237-cnf-test-results.tar.gz"' \
+// --form 'certId="652787"' \
+// --form 'description="aaa"'
+
+func SendResultsToConnectAPI(zipFile, apiKey, projectID string) error {
+	log.Info("Sending results to Red Hat Connect")
+
+	var (
+		buf = new(bytes.Buffer)
+	)
+
+	// Create a new multipart writer
+	w := multipart.NewWriter(buf)
+	defer w.Close()
+
+	_, err := w.CreateFormFile("attachment", filepath.Base(zipFile))
+	if err != nil {
+		return fmt.Errorf("failed to create form file: %v", err)
+	}
+
+	// Create a form field
+	err = createFormField(w, "type", "RhocpBestPracticeTestResult")
+	if err != nil {
+		return err
+	}
+
+	// Create a form field
+	err = createFormField(w, "certId", projectID)
+	if err != nil {
+		return err
+	}
+
+	// Create a form field
+	err = createFormField(w, "description", "CNF Test Results")
+	if err != nil {
+		return err
+	}
+
+	// Create a new request
+	req, err := http.NewRequest("POST", connectAPIURL, buf)
+	if err != nil {
+		return fmt.Errorf("failed to create new request: %v", err)
+	}
+
+	// Set the content type
+	req.Header.Set("Content-Type", w.FormDataContentType())
+	req.Header.Set("x-api-key", apiKey)
+
+	// Create a client
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to send post request to the endpoint: %v", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to send post request to the endpoint: %v", res.Status)
 	}
 
 	return nil
--- pkg/certsuite/certsuite.go
+++ pkg/certsuite/certsuite.go
@@ -128,7 +128,7 @@ func Shutdown() {
 	}
 }
 
-//nolint:funlen
+//nolint:funlen,gocyclo
 func Run(labelsFilter, outputFolder string) error {
 	testParams := configuration.GetTestParameters()
 
@@ -201,11 +201,34 @@ func Run(labelsFilter, outputFolder string) error {
 	// Add the log file path
 	allArtifactsFilePaths = append(allArtifactsFilePaths, filepath.Join(outputFolder, log.LogFileName))
 
+	// Red Hat Connect API key and project ID are required to send the tar.gz to Red Hat Connect.
+	sendToConnectAPI := false
+	if configuration.GetTestParameters().ConnectAPIKey != "" && configuration.GetTestParameters().ConnectProjectID != "" {
+		sendToConnectAPI = true
+	}
+
 	// tar.gz file creation with results and html artifacts, unless omitted by env var.
-	if !configuration.GetTestParameters().OmitArtifactsZipFile {
-		err = results.CompressResultsArtifacts(resultsOutputDir, allArtifactsFilePaths)
+	if !configuration.GetTestParameters().OmitArtifactsZipFile || sendToConnectAPI {
+		zipFile, err := results.CompressResultsArtifacts(resultsOutputDir, allArtifactsFilePaths)
 		if err != nil {
 			log.Fatal("Failed to compress results artifacts: %v", err)
+		}
+
+		if sendToConnectAPI {
+			err = results.SendResultsToConnectAPI(zipFile,
+				configuration.GetTestParameters().ConnectAPIKey,
+				configuration.GetTestParameters().ConnectProjectID)
+			if err != nil {
+				log.Fatal("Failed to send results to Red Hat Connect: %v", err)
+			}
+		}
+
+		if !configuration.GetTestParameters().OmitArtifactsZipFile {
+			// delete the zip as the user does not want it.
+			err = os.Remove(zipFile)
+			if err != nil {
+				log.Fatal("Failed to remove zip file %s: %v", zipFile, err)
+			}
 		}
 	}
 
